481b7bee4fbdea7de7624f7d8f9ea98789d950f7
subhamse <129133175+subhamse@users.noreply.github.com>
2025-04-11 18:53:18 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello
 {
 Public static void main(String args[])
 {
-System.out.println(“third commit changing for new-branch”);
+System.out.println(“third commit changing for new-branch and it is good”);
 }
 }
 
